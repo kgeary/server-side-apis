0ece29634bb8f02fc840dbfd1aeb36bd348adc9e
Kevin Geary <kgeary@gmail.com>
2019-12-18 21:36:10 -0600

    Working ajax

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,7 @@
 const $fiveDay = $(".five-day");
 const cities = JSON.parse(localStorage.getItem("cities")) || ["Austin", "Detroit", "Los Angeles"];
-
+const API_BASE_URL = "http://api.openweathermap.org/data/2.5/";
+const METERS_PER_SEC_TO_MPH = 2.23693629;
 function populateCities() {
     // Populate the cities list
     let $cityGroup = $('#city-group');
@@ -39,21 +40,79 @@ function loadCity(city) {
     // Set the current Humidity
     // Set the current Windspeed
     // Set the curren UV-Index
-    load5DayForecast();
+    load5DayForecast(city);
 }
 
-function load5DayForecast() {
+function load5DayForecast(city) {
     // Clear out old 5-day
     $fiveDay.html("");
+    getData(city);
+}
+
+function getData(city) {
+    
+    let current_api_url = getApiUrl(city, false);
+    let forecast_api_url = getApiUrl(city, true);
     let startMoment = moment().clone();
-    for(let i=0; i<5; i++) {
-        let date = startMoment.add(1, "day");
 
-        // API CALL HERE
+    $.ajax({
+        url:  current_api_url,
+        method: "GET",
+    }).then(function(response) {
+        console.log("Current Response Received");
+        console.log(response);
+        let condition = response.weather[0].main + " - " + response.weather[0].description;
+        let tempF = response.main.temp;
+        let humidity = response.main.humidity;
+        let windSpeed = response.wind.speed;
+        let windDir = response.wind.deg;
+        let coord = response.coord;
+        let sunrise = moment.unix(response.sys.sunrise).format("h:mma");
+        let sunset = moment.unix(response.sys.sunset).format("h:mma");
+        $("#head-city-date").text(response.name + moment.unix(response.dt).format(" (M/DD/YYYY)"));
+        $("#temperature").html(tempF.toFixed(0) + "&deg;F");
+        $("#humidity").text(humidity.toFixed(0)+ "%");
+        $("#wind-speed").text((windSpeed).toFixed(1)+ "mph");
+        // TODO - Update Image
+        $("#uv-index").text(condition); // TODO - UV Index
+        $("#sunrise").text(sunrise); // TODO - UV Index
+        $("#sunset").text(sunset); // TODO - UV Index
 
-        let card = create5DayCard(date, i*20, i+70);
-        $fiveDay.append(card);
-    }
+    });
+
+    // API CALL HERE
+    $.ajax({
+        url:  forecast_api_url,
+        method: "GET",
+    }).then(function(response) {
+        console.log("Forecast Response Received");
+        console.log(response);
+
+        for(let i=0; i<5; i++) {
+            let li = response.list[i*8];
+            let timeStamp = moment.unix(li.dt);
+            let humidity = li.main.humidity;;
+            let tempF = li.main.temp;
+            let icon = li.weather[0].icon;
+            let description = li.weather[0].description;
+            // Pull data from response here
+            let date = startMoment.add(1, "day");
+            // TODO UPDATE IMAGE
+            let card = create5DayCard(date, tempF.toFixed(0), humidity.toFixed(0));
+            $fiveDay.append(card);
+        }
+    });
+}
+
+// Get the Final API Url
+// city to query
+// isForecast = True for 5-day forecast, false for current weather
+function getApiUrl(city, isForecast) {
+    let queryString = isForecast ? "forecast" : "weather";
+    queryString += "?";
+    queryString += "q=" + city.trim().replace(" ", "+") + ",us&mode=json";
+    queryString += "&units=imperial" + "&APPID=" + API_KEY;
+    return API_BASE_URL + queryString;
 }
 
 function create5DayCard(momentDay, temperature, humidity) {
